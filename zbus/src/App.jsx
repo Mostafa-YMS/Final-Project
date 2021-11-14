691c6e2cece97eabb5101fed00ec7228907ac546
ayyaehab <ayyaehab66@gmail.com>
2021-11-14 21:30:44 +0200

    test

--- zbus/src/App.jsx
+++ zbus/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
-
+import Home from './Home';
 function App() {
   //ToDo
+  return (
+    <div className="App">
+       <div className="content">
+         <Home />
+       </div>
+    </div>
+  );
 }
 
 export default App;

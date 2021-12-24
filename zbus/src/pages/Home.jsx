9abbfdfd0d593acb41c0551cadd08d796b11b2d6
anas <anasrafat600@gmail.com>
2021-12-24 23:59:18 +0200

    home

--- zbus/src/pages/Home.jsx
+++ zbus/src/pages/Home.jsx
@@ -17,23 +17,45 @@ const Home = () => {
     navigate("/Login");
   };
   return (
+    // <div>
+    //   <div className='container'>
+    //     <div className='row'>
+    //       <div className='col-6'>
+    //       <p style={{ color: "white",fontSize: "50pt"  }}> UBus</p>
+          
+    //     </div>
+        
+    //       <div className='col-6'>
+    //       <button className="btn btn-dark" onClick={Reg}  style={{ float: "right",display:"flex" }}>Register</button>
+    //       <button className="btn btn-secondary" onClick={Log} style={{ float: "right",display:"flex" }}>Login</button>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <br></br><br></br><br></br>
+    //   <h1 style={{ color: "white", textAlign: "center",fontFamily:"-moz-initial" }}>We take you throuh the ultimate journey</h1>
+
+
+    // </div>
     <div>
       <div className='container'>
         <div className='row'>
-          <div className='col-6'>
+          <div className='col-12'>
           <p style={{ color: "white",fontSize: "50pt"  }}> UBus</p>
           
         </div>
         
-          <div className='col-6'>
-          <button className="btn btn-dark" onClick={Reg}  style={{ float: "right",display:"flex" }}>Register</button>
-          <button className="btn btn-secondary" onClick={Log} style={{ float: "right",display:"flex" }}>Login</button>
-          </div>
         </div>
       </div>
-      <br></br><br></br><br></br>
-      <h1 style={{ color: "white", textAlign: "center",fontFamily:"-moz-initial" }}>We take you throuh the ultimate journey</h1>
-
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+        
+      <h1 style={{ color: "#ffffff", textAlign: "center",fontFamily:"-moz-initial" }}>We take you throuh the ultimate journey</h1>
+      <br></br><br></br>
+      <div style={{  textAlign: "center"}}>
+      <button className="btn btn-light" onClick={Log} style={{ width:"150px",margin:"10px" }}>Login</button>
+          <button className="btn btn-secondary" onClick={Reg}  style={{ width:"150px",margin:"10px" }}>Register</button>
+          
+          </div>
+      
 
     </div>
   )
